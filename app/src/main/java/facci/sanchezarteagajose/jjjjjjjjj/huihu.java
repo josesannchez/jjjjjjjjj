44a90c20293c7a543d934b920f125e9aa7cedc00
j San <jsan1741@gmail.com>
2018-11-27 05:48:06 -0500

    qmasv

---
+++ app/src/main/java/facci/sanchezarteagajose/jjjjjjjjj/huihu.java
@@ -0,0 +1,13 @@
+package facci.sanchezarteagajose.jjjjjjjjj;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class huihu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_huihu);
+    }
+}
